1dde4d7eb705370ea3352d432862bbac97fdf0b4
Denys GONOR <dgonor@e1r2p14.unit.ua>
2018-05-24 10:17:59 +0300

    Thu May 24 10:17:59 EEST 2018

--- ft_printf.c
+++ ft_printf.c
@@ -24,11 +24,11 @@ int			ft_printf(const char *format, ...)
 	va_start(arg, format);
 	while (format[++i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && !(ft_strequ(format, "% ")))
 			res += ft_pars_printf((format + (i += 1)), &i, &arg);
 		if (!format[i])
 			break ;
-		if (format[i] == '%')
+		if (format[i] == '%' && !(ft_strequ(format, "% ")))
 		{
 			i--;
 			continue ;
